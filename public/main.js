4b16ef4e1a5a5818aef0d8c4a6d07e9c61dc2c0f
benjaminr177 <unoceptik@gmail.com>
2017-11-21 10:27:40 +0200

    Added graph functionality

--- public/main.js
+++ public/main.js
@@ -10,6 +10,47 @@ var profile = () => {
   cashEl.innerText = 'You have ' + numberWithCommas(cashAmount) + '$';
 }
 
+var graph = (data) => {
+  var ifImageExists = document.querySelectorAll('img')
+  var quoteCard = document.querySelector('.quote')
+
+  if (ifImageExists.length > 0) {
+    quoteCard.removeChild(ifImageExists[0])  
+    quoteCard.removeChild(document.querySelector('.button-wrapper'))
+  }
+
+  var img = document.createElement('img')
+  img.classList.add('graph')
+  img.src = `https://finance.google.com/finance/getchart?q=${data.symbol}&p=1M`
+
+  var buttonDay = document.createElement('button')
+  var buttonMonth = document.createElement('button')
+  var buttonYear = document.createElement('button')
+  buttonYear.innerText = 'Year'
+  buttonYear.addEventListener('click', () => {
+    img.src = `https://finance.google.com/finance/getchart?q=${data.symbol}&p=1Y`
+  })
+  buttonMonth.innerText = 'Month'
+  buttonMonth.addEventListener('click', () => {
+    img.src = `https://finance.google.com/finance/getchart?q=${data.symbol}&p=1M`
+  })
+  buttonDay.innerText = '5 Days'
+  buttonDay.addEventListener('click', () => {
+    img.src = `https://finance.google.com/finance/getchart?q=${data.symbol}&p=5d`
+  })
+
+  var buttonWrapper = document.createElement('div')
+  buttonWrapper.classList.add('button-wrapper')
+  
+  quoteCard.appendChild(img)
+  quoteCard.appendChild(buttonWrapper)
+
+  var bw = document.querySelector('.button-wrapper')
+  bw.appendChild(buttonDay)
+  bw.appendChild(buttonMonth)
+  bw.appendChild(buttonYear)
+}
+
 var stockTable = () => {
   var table = document.querySelector('.profile > table')
 
@@ -41,6 +82,7 @@ var quote = () => {
       .then(data => data.json())
       .then(json => {
         result.innerText = json.symbol + ': ' + json.price + '$'
+        graph(json)
       })
   })
 }
